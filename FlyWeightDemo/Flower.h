29626061677aa34988cac141fd94c0a73bebecbf
GengbinZ <30311307+GengbinZ@users.noreply.github.com>
2018-08-24 17:57:26 +0800

    享元模式

---
+++ FlyWeightDemo/Flower.h
@@ -0,0 +1,14 @@
+//
+//  Flower.h
+//  FlyWeightDemo
+//
+//  Created by James on 2017/12/5.
+//  Copyright © 2017年 TZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Flower : NSObject
+@property (nonatomic, copy) NSString *flowerColor; //  花色
+@property (nonatomic, copy) NSString *flowerName; // 花名
+@end
---
+++ FlyWeightDemo/Flower.m
@@ -0,0 +1,13 @@
+//
+//  Flower.m
+//  FlyWeightDemo
+//
+//  Created by James on 2017/12/5.
+//  Copyright © 2017年 TZ. All rights reserved.
+//
+
+#import "Flower.h"
+
+@implementation Flower
+
+@end
